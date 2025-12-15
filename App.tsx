84d5641715533ddeb1943ce6da2600bb9a56df0f
Siya-Tonjeni <tonjenisiya@gmail.com>
2025-12-15 13:40:33 +0200

    Changes made

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 import { StyleSheet, Text, View } from 'react-native';
 import ValueData from './src/components/Value';
 import RingProgress from './src/components/RingProgress';
@@ -6,7 +7,7 @@ import RingProgress from './src/components/RingProgress';
 export default function App() {
   return (
     <View style={styles.container}>
-      <RingProgress progress={0.25} />
+      <RingProgress progress={1} />
 
       <View style={styles.values}>
         <ValueData label="Steps" value="129" />
--- src/components/RingProgress.tsx
+++ src/components/RingProgress.tsx
@@ -1,5 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 import SVG, {Circle} from 'react-native-svg';
+import Animated, {useAnimatedProps, useSharedValue, withTiming} from 'react-native-reanimated'
+import { useEffect } from 'react';
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 type RingProgressProps = {
     radius?: number;
@@ -12,6 +16,16 @@ const colour = "#EE0F55"
 const RingProgress = ({radius = 100, strokeWidth = 35, progress}: RingProgressProps) => {
     const innerRadius = radius - strokeWidth / 2;
     const circumference = 2 * Math.PI * innerRadius
+
+    const fill = useSharedValue(0);
+
+    useEffect(() => {
+        fill.value = withTiming(progress, {duration: 1500})
+    }, [progress]);
+
+    const animatedProps = useAnimatedProps(() => ({
+        strokeDasharray : [circumference * fill.value, circumference]
+    }))
     
     return (
         <View style={{width: radius * 2, height: radius * 2, alignSelf: 'center'}}>
@@ -28,13 +42,13 @@ const RingProgress = ({radius = 100, strokeWidth = 35, progress}: RingProgressPr
                 />
 
                 {/* Foreground */}
-                <Circle 
+                <AnimatedCircle
+                animatedProps={animatedProps}
                 cx={radius} 
                 cy={radius} 
                 r={innerRadius} 
                 strokeWidth={strokeWidth}
                 stroke={colour}
-                strokeDasharray={[circumference * progress, circumference]}
                 strokeLinecap='round'
                 rotation="-90"
                 originY={radius}
